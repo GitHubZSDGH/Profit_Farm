b3bbbf2a8671a18c5808e629519a6cfa2b524066
FromChainStudent <1379587022@qq.com>
2018-05-06 18:52:20 +0800

    分包

---
+++ app/src/main/java/com/profitfarm/hlks/profit_farm/activity/Test.java
@@ -0,0 +1,15 @@
+package com.profitfarm.hlks.profit_farm.activity;
+
+/**
+ * 项目名称: WanAndroid
+ * 类描述:
+ * 创建人: YC
+ * 创建时间: 2018/5/6 18:48
+ * 修改人:
+ * 修改内容:
+ * 修改时间:
+ */
+
+
+public class Test {
+}
---
+++ app/src/main/java/com/profitfarm/hlks/profit_farm/activity/info/OrderDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.profitfarm.hlks.profit_farm.activity.info;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.profitfarm.hlks.profit_farm.R;
+
+public class OrderDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_details);
+    }
+}
